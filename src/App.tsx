41ab0bfd286f120765d2cc21e3a81fe0d564c887
Yasharm4x <yash4rma@gmail.com>
2026-01-12 19:58:44 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import { VerificationPage } from "./components/VerificationPage";
 import NotFound from "./pages/NotFound";
 
-const queryClient = new QueryClient();
-
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/verify" element={<VerificationPage />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </TooltipProvider>
-  </QueryClientProvider>
+  <Routes>
+    <Route path="/" element={<Index />} />
+    <Route path="/verify" element={<VerificationPage />} />
+    <Route path="*" element={<NotFound />} />
+  </Routes>
 );
 
 export default App;
