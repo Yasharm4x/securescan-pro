acd1a1779c415aa9b05bb567a1e7358364b21021
Yasharm4x <yash4rma@gmail.com>
2026-01-12 20:30:34 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,22 @@ import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={<div style={{ padding: 40 }}>HOME OK</div>}
-      />
-      <Route
-        path="/verify"
-        element={<div style={{ padding: 40 }}>VERIFY OK</div>}
-      />
-      <Route
-        path="*"
-        element={<div style={{ padding: 40 }}>NOT FOUND</div>}
-      />
-    </Routes>
+    <div style={{ minHeight: "100vh", background: "#0f172a", color: "white" }}>
+      <Routes>
+        <Route
+          path="/"
+          element={<div style={{ padding: 40 }}>HOME OK</div>}
+        />
+        <Route
+          path="/verify"
+          element={<div style={{ padding: 40 }}>VERIFY OK</div>}
+        />
+        <Route
+          path="*"
+          element={<div style={{ padding: 40 }}>NOT FOUND</div>}
+        />
+      </Routes>
+    </div>
   );
 };
 
